b3b8854dd2acfb0c720dfe89a62a0deafcb45516
Ike85 <145978642+Ike85@users.noreply.github.com>
2026-04-15 16:06:35 +0100

    Fix

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import *
-
-admin.site.register(CustomUser)
-admin.site.register(Project)
-admin.site.register(ProjectMember)
-admin.site.register(Task)
-admin.site.register(TaskComment)
