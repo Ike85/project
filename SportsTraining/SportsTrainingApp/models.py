1f680ad28a30e1fd9dfbe8167b7ed96e369a70ba
Ike85 <145978642+Ike85@users.noreply.github.com>
2026-04-15 16:35:31 +0100

    Models and Django UI

--- SportsTraining/SportsTrainingApp/models.py
+++ SportsTraining/SportsTrainingApp/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Student(models.Model):
+    student_id = models.IntegerField(primary_key=True)
+    studentName = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+class Module(models.Model):
+    module_id = models.IntegerField(primary_key=True)
+    moduleTitle = models.CharField(max_length=100)
+    moduleDescription = models.CharField(max_length=100)
+    moduleStatus = models.CharField(max_length=100)
